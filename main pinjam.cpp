844e8230a66a7b879932afc590d735b2525f05b4
DiftaFitrahul <88217314+DiftaFitrahul@users.noreply.github.com>
2021-12-11 00:47:58 +0700

    Create main pinjam.cpp

---
+++ main pinjam.cpp
@@ -0,0 +1,11 @@
+#include"pinj.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+int main(){
+	showHomescreen();
+}
+//Untuk panggil program tinggal ketik function diatas
+//dan headernya
